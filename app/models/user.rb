e771ebd174571f08d35660083f88eab2c27f99d4
So.I <main@sokz.net>
2025-04-23 13:51:00 +0900

    add: L0 C31.11 まで完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reports, dependent: :destroy
+
+  # role が 0 なら管理者
+  def admin?
+    role == 0
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.name = "Admin"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.role = 0
+end
