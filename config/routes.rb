17235c126ecbaf4097416dd2acc679aa7c1edb56
So.I <main@sokz.net>
2025-04-23 13:53:44 +0900

    add: L0 C31.13まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
- root 'home#index'
+  devise_for :users
+  root "home#index"
+
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :reports
+
+  namespace :admin do
+    root "dashboard#index"
+    resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :reports, only: [:index, :show, :destroy]
+  end
 end
